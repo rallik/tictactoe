d84d7ac6797e8495e1509ae0e4973a56b0a04d1b
Reuben T Allik <reubenallik@gmail.com>
2021-02-22 21:36:22 -0500

    responsive

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+// function newgame() {
+//     const newgamebutton = document.getElementById('boardbutton');
+
+// }
+
 function whowins(winner) {
-    console.log(`Player ${winner} wins!!`)
-    const wintext = document.getElementById("text")
+    console.log(`${winner}'s win!!`);
+    const wintext = `${winner}'s win!!`;
+    const wincontainer = document.getElementById("text");
+    // const buttonswitch = document.getElementById('boardbutton');
+    wincontainer.innerHTML = wintext;
+    // buttonswitch.innerHTML = "New Game";
+}
+
+function itsatie() {
+    const tiecontainer = document.getElementById("text");
+    tiecontainer.innerHTML = "It's a tie!!";
 }
 
 //check board for win
 function checkboard() {
     if (count<5) {
-        console.log("Can't win yet")
+        console.log("Can't win yet");
     } else {
         const wins = [
             [0,1,2],
@@ -29,17 +43,21 @@ function checkboard() {
             place1 = currentboard[win[0]];
             place2 = currentboard[win[1]];
             place3 = currentboard[win[2]];
-            console.log('current places tested', place1, place2, place3)
+            console.log('current places tested', place1, place2, place3);
 
-            if (place1==place2 && place2==place3 && place1==place3) {
-                console.log("Win")
-                callfireworks()
+            if (place1!="" && place1==place2 && place2==place3 && place1==place3) {
+                console.log("Win");
+                whowins(place1)
+                break;
+            } else if (currentboard.indexOf("")==-1) {
+                console.log("tie");
+                itsatie()
                 break;
             } else {
                 continue;
             }
         }
-        console.log(currentboard)
+        console.log(currentboard);
     }
 
 }
@@ -50,7 +68,7 @@ let turn = 'X';
 let count = 0;
 
 document.querySelectorAll('.tile').forEach(tile => {
-    tile.addEventListener('click', event => {
+    tile.addEventListener('click', function() {
         console.log(tile.innerHTML)
         if (turn==='X' && tile.innerHTML==="") {
             tile.innerHTML = "X";
@@ -69,6 +87,17 @@ document.querySelectorAll('.tile').forEach(tile => {
     })
 })
 
+function clearboard() {
+    const clear = document.getElementById('boardbutton');
+    clear.addEventListener('click', function(){
+        document.querySelectorAll('.tile').forEach(tile =>{
+            tile.innerHTML = "";
+        })
+        document.getElementById("text").innerHTML = "";
+        count = 0;
+        console.log("Board Cleared")
+    })
+}
 
+clearboard()
 
-//click event in each square
